512bf5bbf14846c3d7f1727faf9c519ab4e685e6
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-26 14:09:20 +0900

    리다이렉트로 페이지 보호호

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
-import { Routes, Route } from "react-router-dom";
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(true);
+  //함수가 아니라 컴포넌트임 대문자로 시작하니까
+  const PrivateRoute = () => {
+    return authenticate?<UserPage />:<Navigate to="/login" />
+  }
   return (
     <div>
       <Routes>
@@ -13,6 +21,8 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </div>
   ); 
