e8d1007c7144728b10f91ab97e3dd3f6afb60c90
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-26 13:40:50 +0900

    restful Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
+import ProductPage from './page/ProductPage';
+import ProductDetailPage from './page/ProductDetailPage';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 
@@ -9,9 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </div>
-  );
+  ); 
 }
 
 export default App;
