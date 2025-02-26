c8f76ca4672ba83cc5d5c20586ee5f981378b6c0
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-26 13:59:48 +0900

    useSearchParams :url 쿼리값 읽어오기기

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+    const navigate = useNavigate();
+    const goProductPage = () => {
+        navigate("/products?q=pants");
+    }
   return (
     <div>
         <h1>HomePage</h1>
         <Link to="/about" >Go to about Page</Link>
+        <button onClick={goProductPage}>go to Product</button>
     </div>
   )
 }
--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const ProductPage = () => {
+    let [query, setQuert] = useSearchParams();
+    console.log(query.get("q"));
   return (
     <div>
         <h1>show All Products!!</h1>
