c918d6212b677dd1d4d159316c4bb07b88eeb048
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-26 13:44:45 +0900

    useParams 통해서 parameter 값 받아오기

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetailPage = () => {
+    const {id} = useParams();
   return (
     <div>
-        <h1>productDetailPage</h1>
+        <h1>productDetailPage{id}</h1>
     </div>
   )
 }
